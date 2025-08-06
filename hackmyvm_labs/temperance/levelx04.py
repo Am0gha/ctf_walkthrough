6e1d1ed3e648e727ddf58b8407638ae1124a05ec
Amogha Deshpande <amogh462@gmail.com>
2025-08-06 16:45:31 +0530

    Sending the reverse of the string.

---
+++ hackmyvm_labs/temperance/levelx04.py
@@ -0,0 +1,31 @@
+import socket
+import base64
+
+HOST = "temperance.hackmyvm.eu"
+PORT = 9988
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.connect((HOST, PORT))
+
+    # Connect to the host and receive the message / Conecta al host y recibes la intro general.
+    print('Receiving Intro')
+    data = s.recv(1024)
+    print(data)
+
+    # Send "levelx00" to choose the level / Envia levelx00 para elegir el nivel.
+    s.send(b'levelx04')
+
+    # Receive the challenge / Recibe el challenge.
+    print('Receiving challenge.')
+    data2 = s.recv(1024)
+    print(data2)
+
+    # Send the challenge solved / Envia el resultado del challenge.
+    print('Sending the challenge')
+    s.send(data2[::-1])
+
+    # Receive the flag / Recibe la flag.
+    print('Receiving the flag')
+    data3 = s.recv(1024)
+    print(data3)
+
