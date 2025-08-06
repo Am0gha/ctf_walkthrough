6f2408ee442d83de5eaa8eab81604e79106b7f99
Amogha Deshpande <amogh462@gmail.com>
2025-08-06 16:52:16 +0530

    Parsing the input from strings to numbers and sending the sum of the numbers.

---
+++ hackmyvm_labs/temperance/levelx08.py
@@ -0,0 +1,37 @@
+import socket
+import base64
+
+HOST = "temperance.hackmyvm.eu"
+PORT = 9988
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.connect((HOST, PORT))
+
+    # Connect to the host and receive the message / Conecta al host y recibes la intro general.
+    print('Receiving Intro')
+    data = s.recv(1024)
+    print(data)
+
+    # Send "levelx00" to choose the level / Envia levelx00 para elegir el nivel.
+    s.send(b'levelx08')
+
+    # Receive the challenge / Recibe el challenge.
+    print('Receiving challenge.')
+    data2 = s.recv(1024)
+    print(data2)
+    inpt=data2.decode('utf-8')
+    print(inpt,type(inpt))
+    num1=int(inpt[0])*10+int(inpt[1])
+    num2=int(inpt[-2])*10+int(inpt[-1])
+    res=num1+num2
+    res=str(res)
+    res=res.encode('utf-8')
+    print(res,type(res))
+
+    # Send the challenge solved / Envia el resultado del challenge.
+    print('Sending the challenge')
+    s.send(res)
+    # Receive the flag / Recibe la flag.
+    print('Receiving the flag')
+    data3 = s.recv(1024)
+    print(data3)
